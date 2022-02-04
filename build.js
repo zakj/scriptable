466cae4316c6d29b757a9146a7e867000ad7dca4
Zak Johnson <me@zakj.net>
2022-02-03 22:53:49 -0800

    Improve build process, add banners

---
+++ build.js
@@ -0,0 +1,19 @@
+const { build } = require("esbuild");
+
+const entries = {
+  "Info.ts": "// icon-color: blue; icon-glyph: info;",
+  "Obsidian.ts": "// icon-color: deep-gray; icon-glyph: tasks;",
+};
+
+const watch = process.argv.includes("-w") || process.argv.includes("--watch");
+for (const [entry, banner] of Object.entries(entries)) {
+  build({
+    banner: { js: banner },
+    bundle: true,
+    entryPoints: [entry],
+    logLevel: "info",
+    minify: true,
+    outdir: "Documents",
+    watch,
+  });
+}
